b54fe7140e7327f2f1c9d5a3f43d3e05cae9f10d
grady.liu <grady0902@gmail.com>
2024-06-15 23:30:31 +0800

    feat: Add order module and routing

--- projects/app3-orders/src/app/app-routing.module.ts
+++ projects/app3-orders/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'order', loadChildren: () => import('./order/order.module').then(m => m.OrderModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ projects/app3-orders/src/app/order/order-list/order-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pmo-order-list',
+  templateUrl: './order-list.component.html',
+  styleUrls: ['./order-list.component.scss']
+})
+export class OrderListComponent {
+
+}
