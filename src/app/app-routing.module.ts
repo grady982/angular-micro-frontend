334c284fdae1221bb838ebcdf3a9b09ee608e5a9
grady.liu <grady0902@gmail.com>
2024-06-16 09:04:58 +0800

    chore: Update routing configuration for lazy loading of modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
 ];
 
 @NgModule({
